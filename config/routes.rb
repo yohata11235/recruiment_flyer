fdd3837e19e080cc5c83c2c3255f84893ac5a324
yohata11235 <sakinakidev@gmail.com>
2020-08-30 10:15:31 +0000

    20200830

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'offers/index'
+  get 'offers/new'
+  get 'offers/show'
+  get 'offers/edit'
   get 'users/index'
   get 'users/show'
   get 'users/edit'
@@ -8,5 +12,6 @@ Rails.application.routes.draw do
   get 'homes/edit'
   get 'homes/new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+root 'homes#index'
 
 end
--- db/migrate/20200830034822_create_users.rb
+++ db/migrate/20200830034822_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.String :username
-      t.String :company
-      t.String :name
-      t.int :number
+      t.string :username
+      t.string :company
+      t.string :name
+      t.integer :number
 
       t.timestamps
     end
